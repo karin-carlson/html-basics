de903272e56a42a7759fd861b96f0e92df0f101a
karin carlson <kacarlson2489@gmail.com>
2018-08-18 15:20:00 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+  </div>
+<div id="sticky-nav" class="header__wrapper">
+  <!--class is a command I don't initially recognize. I looked it up and it's
+  used to group similar elements that should be uniform but different from <thead>
+    main text !-->
+  </thead>
+  <header class="site-header" role="banner">
+    <div class="wrapper nav-wrapper">
+      <div class="nav--desktop">
+        <div class="mobile-wrapper">
+<div class="header-cart__wrapper hidden-lg">
+  <a href="/cart" class="CartToggle header-cart"><i class="icon icon-ra_bag"></i></a>
+  <span class="header-cart__bubble cartCount has-items cartCount-updater  hidden-count">0</span>
+</div>
+<div class="logo-wrapper ">
+
+    <h1 class="h4 header-logo" itemscope itemtype="http://schema.org/Organization">
+
+
+      <a href="/" itemprop="url"><i class="icon icon-ra_primary-logo"></i></a>
+      (!--item prop is new to me, I'm assuming it has something to do with
+       commerce!--)
+  </body>
+</html>
+# fundamentals
+projects
